2447bebbe73fe2435ad2041440c06920ea75fe6c
Anthony Zheng <tamakimouto@gmail.com>
2020-01-09 22:35:36 -0500

    Updated packages, divided the transfer of blog posts, add scroll

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import format from 'date-fns/format';
+import formatRFC3339 from 'date-fns/formatRFC3339';
+import Scrollbox from 'react-perfect-scrollbar';
 
 const apiKey = "AIzaSyBoMgkuecoKhgTxc5me-wTnh1IYd-ZI9qU";
 const blogId = "8723880054075980249";
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
     root: {
+        display: "flex",
+        height: "calc(100vh - 100px)",
+        justifyContent: "center"
+    },
+    wrapper: {
+        width: "95vw",
         display: "flex",
         flexDirection: "column",
         alignItems: "center"
@@ -21,39 +29,40 @@ const styles = theme => ({
             marginBottom: 0
         }
     }
-});
+}));
 
-class Blog extends React.Component {
+const Blog = (props) => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: []
-        };
-    }
+    const classes = useStyles();
+    const [posts, setPosts] = useState([]);
+    const [datePointer, setDatePointer] = useState(formatRFC3339(new Date()));
 
-    componentDidMount() {
-        axios.get(`https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts?key=${apiKey}&fields=items(published, title, content)`)
+    useEffect(() => {
+        axios.get(`https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts?key=${apiKey}&fields=items(published, title, content)&maxResults=50&endDate=${datePointer}`)
             .then(res => {
-                this.setState({ posts: res.data.items });
+                setPosts(res.data.items);
             });
+    }, [datePointer]);
+
+    const fetchNewSet = () => {
+        setDatePointer(posts[posts.length - 1].published);
     }
 
-    render() {
-        const { classes } = this.props;
-
-        return (
-            <div className={classes.root}>
-                {this.state.posts.map(post => (
-                    <div key={post.id} className={classes.post}>
-                        <h2>{post.title}</h2>
-                        <span>{format(new Date(post.published), "MMM Do, YYYY")}</span>
-                        <p>{post.content}</p>
-                    </div>
-                ))}
-            </div>
-        );
-    }
+    return (
+        <div className={classes.root}>
+            <Scrollbox>
+                <div className={classes.wrapper}>
+                    {posts.map(post => (
+                        <div key={post.id} className={classes.post}>
+                            <h2>{post.title}</h2>
+                            <span>{format(new Date(post.published), "MMM Do, yyyy - hh:mm a")}</span>
+                            <p>{post.content}</p>
+                        </div>
+                    ))}
+                </div>
+            </Scrollbox>
+        </div>
+    );
 }
 
-export default withStyles(styles)(Blog);
+export default Blog;
